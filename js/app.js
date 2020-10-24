5ce907430074dff96ce6bd85067614354379f3dd
Oskar Floeck <s3725028@student.rmit.edu.au>
2020-10-24 13:41:51 +0930

    :fire: Removing unnecessary files. Updating styles

--- js/app.js
+++ js/app.js
@@ -57,7 +57,7 @@ particlesJS('particles-js',
       },
       "move": {
         "enable": true,
-        "speed": 6,
+        "speed": 3,
         "direction": "none",
         "random": false,
         "straight": false,
